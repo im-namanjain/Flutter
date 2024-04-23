e8fd588deb44983ff749c4f55718b10845793946
im-namanjain <namanjainalwar12@gmail.com>
2024-04-23 22:48:44 +0530

    Added otppage

--- balley_messaging_app/lib/loginpage.dart
+++ balley_messaging_app/lib/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:balley_messaging_app/otppage.dart';
 import 'package:flutter/material.dart';
 
 class loginpage extends StatefulWidget {
@@ -8,8 +9,91 @@ class loginpage extends StatefulWidget {
 }
 
 class _loginpageState extends State<loginpage> {
+  var emailtext = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.white,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 35,
+            ),
+            Text(
+              "Balley",
+              style: TextStyle(color: Colors.black, fontSize: 18),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: 360,
+              height: 350,
+              child: Image.asset(
+                "assets/images/one.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 340,
+              child: Text(
+                "Balley is free forever. No ads and subscription fees",
+                style: TextStyle(
+                  fontSize: 29,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: 330,
+              child: TextField(
+                controller: emailtext,
+                decoration: InputDecoration(
+                    hintText: "Email Address",
+                    hintStyle: TextStyle(color: Colors.blueGrey),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      color: Colors.blueGrey,
+                    )),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      color: Colors.blueGrey,
+                    ))),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 55,
+              width: 330,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(30)),
+              child: ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => otppage()));
+                  },
+                  child: Text(
+                    "Send Login Code",
+                    style: TextStyle(color: Colors.white),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ balley_messaging_app/lib/otppage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class otppage extends StatelessWidget {
+  const otppage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
