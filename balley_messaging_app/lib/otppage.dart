8913a3aaca3440182f927e49c01f36ff09426357
im-namanjain <namanjainalwar12@gmail.com>
2024-04-24 23:05:21 +0530

    Last one is loginpage now added otppage

--- balley_messaging_app/lib/otppage.dart
+++ balley_messaging_app/lib/otppage.dart
@@ -1,3 +1,4 @@
+import 'package:balley_messaging_app/termspage.dart';
 import 'package:flutter/material.dart';
 
 class otppage extends StatelessWidget {
@@ -5,6 +6,66 @@ class otppage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.white,
+            child: Column(children: [
+              SizedBox(
+                height: 35,
+              ),
+              Text(
+                "Balley",
+                style: TextStyle(color: Colors.black, fontSize: 18),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Text(
+                      "Enter Verification Code",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "We sent a 4-digit code to .",
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                    Text(
+                      "Please enter it below to continue.",
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                    SizedBox(
+                      height: 100,
+                    ),
+                    Container(
+                      height: 40,
+                      width: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30)),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => termspage()));
+                          },
+                          child: Text(
+                            "Submit Code",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    )
+                  ],
+                ),
+              )
+            ])));
   }
 }
---
+++ balley_messaging_app/lib/termspage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class termspage extends StatelessWidget {
+  const termspage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
